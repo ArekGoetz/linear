99f520cf5dbc75043fd216560801279c179e0ead
Arek Goetz <goetz@sfsu.edu>
2026-03-23 12:32:50 -0700

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-
-export default defineConfig({
-  base: '/linear/fourier/',
-  root: 'fourier',
-  plugins: [svelte()],
-});
